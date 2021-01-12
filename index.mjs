b5712cd4005d47a36c8713050feeb150c8d335f1
Markus <Markus@SUPERCELL>
2021-01-12 15:51:46 +0100

    More testing

--- index.mjs
+++ index.mjs
@@ -7,5 +7,9 @@ setInterval(() => {
     tmbtled.setRpm(Math.floor(Math.random() * 14001));
     tmbtled.setTime(Math.floor(Math.random() * 5001), true);
     tmbtled.setRevLights(Math.floor(Math.random() * 101));
+    tmbtled.setGear(Math.floor(Math.random() * 10));
+    tmbtled.setFlashingYellow(true);
+    tmbtled.setFlashingBlue(true);
+    tmbtled.setFlashingRed(true);
 }, 1000 / 60);
 
