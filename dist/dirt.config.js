b5dab29c14906cd01660ae14c8885679db0e5792
Markus <Markus@SUPERCELL>
2022-02-03 21:11:15 +0100

    Updated libusb

--- dist/dirt.config.js
+++ dist/dirt.config.js
@@ -1,3 +1,9 @@
+/**
+ * Dirt config - add or delete mode for each display as you like and make a backup before overwriting with new version
+ * 
+ * Available modes: "SPEED", "RPM", "BRAKETEMP", "LAPTIME", "LAST LAP", "DISTANCE", "POSITION", "LAP", "LAPS LEFT"
+ */
+
 const config = {
     port: 20777,
     leftModes: ["SPEED", "RPM", "BRAKETEMP"],
--- dist/ets2.config.js
+++ dist/ets2.config.js
@@ -1,3 +1,9 @@
+/**
+ * Euro Truck Simulator config - add or delete mode for each display as you like and make a backup before overwriting with new version
+ * 
+ * Available modes: "SPEED", "RPM", "FUEL", "WATERTEMP", "OILTEMP", "OILPRESS", "AIRPRESS", "BRAKETEMP", "DISTANCE", "TIME", "SPEED LIMIT", "CRUISE CONT", "AVG CONSUM", "RANGE" 
+ */
+
 const config = {
     leftModes: ["SPEED", "RPM", "FUEL", "WATERTEMP", "OILTEMP", "OILPRESS", "AIRPRESS", "BRAKETEMP"],
     rightModes: ["DISTANCE", "TIME", "SPEED LIMIT", "CRUISE CONT", "AVG CONSUM", "RANGE"]
